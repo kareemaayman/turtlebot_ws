fa700e2f827f2034004517a281e061d2d0413828
Kareema Barouma <es-karima.ayman2027@alexu.edu.eg>
2024-09-09 23:36:54 +0300

    workspace made and added shato-robot aas submodule

---
+++ build/shato-robot/miarobot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kareema/turtlebot_ws/src/shato-robot/miarobot_description
+# Build directory: /home/kareema/turtlebot_ws/build/shato-robot/miarobot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
